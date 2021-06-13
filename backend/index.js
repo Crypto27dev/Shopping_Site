2307e7cc43257a8697a4d56791c5d28cac9d718f
Upendra <dhamalaupendra@gmail.com>
2021-06-13 08:05:46 +0545

    Ready for deploy

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import path from 'path'
 import orderRoutes from './routes/orderRoutes.js'
 dotenv.config()
 connectDB()
@@ -10,9 +11,18 @@ connectDB()
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.json('successfully implemented backend')
-})
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
